833c559095b4044cadbc5bf313db63008f82e712
Andrew Lundgren <andrew.lundgren@port.ac.uk>
2023-08-13 22:45:05 +0100

    Remove multiprocessing

--- bin/analyse_glitches.py
+++ bin/analyse_glitches.py
@@ -11,7 +11,6 @@ from numpyro import infer
 import arviz as az
 
 numpyro.set_host_device_count(4)
-njobs = 8
 
 from antiglitch import freqs, to_fd, extract_glitch, rfft
 
@@ -19,18 +18,20 @@ from antiglitch import freqs, to_fd, extract_glitch, rfft
 from antiglitch import fsignal
 
 # Bayesian model
-from antiglitch import glitch_model
+from antiglitch import glitch_model, new_model
 
 sampler = infer.MCMC(
-    infer.NUTS(glitch_model),
+    infer.NUTS(new_model, init_strategy=infer.init_to_sample),
     num_warmup=3000,
     num_samples=2000,
-    num_chains=1,
+    num_chains=4,
     progress_bar=False,
+    jit_model_args=True,
 )
 
 # Read all glitches of a certain type from the .npz files
-datadir = sys.argv[1]
+ifo = sys.argv[1]
+datadir = sys.argv[2]
 
 def process(ifo, key, ii, ff):
     npz = np.load(ff)
@@ -45,17 +46,14 @@ def process(ifo, key, ii, ff):
     return ((ifo, key, ii), summ)
 
 simlst = []
-for ifo in ['V1']:
-    for key in ['tomte','blip','koi']:
-        files = sorted(glob(f"{datadir}/{ifo}-{key}-*.npz"))
-        for ii, ff in enumerate(files):
-            simlst.append((ifo, key, ii, ff))
+for key in ['tomte','blip','koi','low_blip']:
+    files = sorted(glob(f"{datadir}/{ifo}-{key}-*.npz"))
+    if not files:
+        continue
+    for ii, ff in enumerate(files):
+        simlst.append((ifo, key, ii, ff))
 
-print(simlst[:10])
-            
-result = Parallel(n_jobs=njobs, verbose=10)(delayed(process)(*arg) for arg in simlst)
+result = dict([process(*arg) for arg in simlst])
 
-result = dict(result)
-
-with open(f"Jul2023-V1-physical-results.pkl", 'wb') as handle:
+with open(f"Aug2023-{ifo}-physical-results.pkl", 'wb') as handle:
     pickle.dump(result, handle, protocol=pickle.HIGHEST_PROTOCOL)
