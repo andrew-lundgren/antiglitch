e5fb50463fa268fa97e629a7a07a380f71ea4d12
Andrew Lundgren <andrew.lundgren@port.ac.uk>
2023-08-16 14:24:52 +0100

    analyse_glitches written for condor

--- bin/analyse_glitches.py
+++ bin/analyse_glitches.py
@@ -18,20 +18,21 @@ from antiglitch import freqs, to_fd, extract_glitch, rfft
 from antiglitch import fsignal
 
 # Bayesian model
-from antiglitch import glitch_model, new_model
+from antiglitch import glitch_model2
 
 sampler = infer.MCMC(
-    infer.NUTS(new_model, init_strategy=infer.init_to_sample),
-    num_warmup=3000,
-    num_samples=2000,
+    infer.NUTS(glitch_model2, init_strategy=infer.init_to_median, dense_mass=True),
+    num_warmup=2000,
+    num_samples=1000,
     num_chains=4,
     progress_bar=False,
     jit_model_args=True,
 )
 
 # Read all glitches of a certain type from the .npz files
-ifo = sys.argv[1]
-datadir = sys.argv[2]
+datadir = sys.argv[1]
+ifo = sys.argv[2]
+key = sys.argv[3]
 
 def process(ifo, key, ii, ff):
     npz = np.load(ff)
@@ -42,18 +43,17 @@ def process(ifo, key, ii, ff):
                     data=fglitch[1:])
 
     itrace = az.from_numpyro(sampler)
-    summ = az.summary(itrace, kind='stats')
+    summ = az.summary(itrace, kind='all', round_to=8)
     return ((ifo, key, ii), summ)
 
 simlst = []
-for key in ['tomte','blip','koi','low_blip']:
-    files = sorted(glob(f"{datadir}/{ifo}-{key}-*.npz"))
-    if not files:
-        continue
-    for ii, ff in enumerate(files):
-        simlst.append((ifo, key, ii, ff))
+files = sorted(glob(f"{datadir}/{ifo}-{key}-*.npz"))
+if not files:
+    exit()
+for ii, ff in enumerate(files):
+    simlst.append((ifo, key, ii, ff))
 
 result = dict([process(*arg) for arg in simlst])
 
-with open(f"Aug2023-{ifo}-physical-results.pkl", 'wb') as handle:
+with open(f"Aug2023v2-{ifo}-{key}-physical-results.pkl", 'wb') as handle:
     pickle.dump(result, handle, protocol=pickle.HIGHEST_PROTOCOL)
