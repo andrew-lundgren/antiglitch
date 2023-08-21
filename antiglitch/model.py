a8e5fc98f97dc10644f6831c77deb7b09671835f
Andrew Lundgren <andrew.lundgren@port.ac.uk>
2023-08-21 22:12:46 +0100

    MLE estimator with normalised template

--- antiglitch/model.py
+++ antiglitch/model.py
@@ -5,6 +5,8 @@ import jax
 import jax.numpy as jnp
 import numpyro
 from numpyro import distributions as dist
+from numpyro.distributions import constraints
+from numpyro.infer import SVI, Trace_ELBO # , autoguide
 from .cplxdist import CplxNormal
 
 #from .utils import center, fglitch_from_sample
@@ -21,6 +23,12 @@ def fsignal(freqs, f0, gbw):
 
 def fglitch_from_sample(amp_r, amp_i, f0, gbw, time, **kwargs):
     ftmp = (amp_r+1.j*amp_i)*fsignal(freqs, f0, gbw)*jnp.exp(-2.j*jnp.pi*(time*freqs))
+    return ftmp
+
+def fglitch_normed(invasd, amp_r, amp_i, f0, gbw, time, **kwargs):
+    raw = invasd*fsignal(freqs, f0, gbw)
+    norm = 1./jnp.sqrt(jnp.sum(jnp.real(raw*raw.conjugate())))
+    ftmp = norm*(amp_r+1.j*amp_i)*raw*jnp.exp(-2.j*jnp.pi*(time*freqs))
     return ftmp
 
 # Bayesian model
@@ -64,3 +72,33 @@ def glitch_model2(freqs, invasd, data=None):
 
     with numpyro.plate("data", len(data)):
         numpyro.sample("y", CplxNormal((amp_r+1.j*amp_i)*jnp.exp(-2.j*jnp.pi*t*freqs)*raw, 0.5), obs=data)
+
+def create_mle_model(freqs, maxamp=10000., progress_bar=False):
+    """MLE model"""
+    def mle_model(freqs, invasd, data=None):
+        amp_r = numpyro.param("amp_r", 0., constraint=constraints.interval(-maxamp, maxamp))
+        amp_i = numpyro.param("amp_i", 0., constraint=constraints.interval(-maxamp, maxamp))
+        amp = numpyro.deterministic("amp", jnp.sqrt(amp_r*amp_r+amp_i*amp_i))
+
+        t = numpyro.param("time", 0., constraint=constraints.interval(-0.01, 0.01))
+        f0 = numpyro.param('f0', 100., constraint=constraints.interval(10., 600.))
+        gbw = numpyro.param('gbw', 1., constraint=constraints.interval(0.25, 8.))
+
+        raw = invasd*fsignal(freqs, f0, gbw)
+        norm = 1./jnp.sqrt(jnp.sum(jnp.real(raw*raw.conjugate()))) 
+
+        with numpyro.plate("data", len(data)):
+            numpyro.sample("y", CplxNormal(norm*(amp_r+1.j*amp_i)*jnp.exp(-2.j*jnp.pi*t*freqs)*raw, 0.5), obs=data)
+
+    def mle_guide(freqs, invasd, data=None):
+        pass
+    
+    def train(data, seed = 101, num_steps = 10000):
+        optimizer = numpyro.optim.Adam(step_size=1e-3)
+        svi = SVI(mle_model, mle_guide, optimizer, loss=Trace_ELBO())
+        svi_result = svi.run(jax.random.PRNGKey(seed), num_steps,
+                                freqs, jnp.array(data['invasd']), jnp.array(data['fdata']),
+                                progress_bar = progress_bar)
+        return svi_result
+    
+    return train
--- antiglitch/utils.py
+++ antiglitch/utils.py
@@ -7,12 +7,9 @@ from functools import partial
 rfft = partial(rfft, norm='ortho')
 irfft = partial(irfft, norm='ortho')
 
-from .model import fglitch_from_sample
+from .model import fglitch_from_sample, fglitch_normed
 
-for path in ['data', '/home/andrew.lundgren/detchar/GlitchSearch/data']:
-    if os.path.isdir(path):
-        datadir = path
-        break
+datadir = '/home/andrew.lundgren/src/antiglitch/notebooks/data/'
 
 def center(data):
     """Shift data from the start to the center of a time series"""
@@ -41,6 +38,34 @@ def extract_glitch(npz, halfwidth=512):
 
     return invasd_ds, whts[len(whts)//2 - halfwidth:len(whts)//2 + halfwidth],  whts
 
+def snr(snip):
+    tmp = np.zeros(4*8192)
+    tmp[:len(snip.glitch)] = snip.glitch.copy()
+    gt = TimeSeries(np.roll(tmp,-len(snip.glitch)//2), delta_t=1/8192)
+    dt = TimeSeries(snip.whts_long, delta_t=1/8192)
+    dt[:4096]=0.;dt[len(dt)-4096:]=0.;
+    snrts = matched_filter(gt, dt, psd=FrequencySeries([1./4096]*(1+4*4906), delta_f=1/4.), low_frequency_cutoff=15)
+
+    return np.abs(snrts[16384-2048:16384+2048]).max()
+    
+def measure(inf_data, snip):
+    itrace = inf_data['mean']
+    inf = {key: float(itrace[key])
+            for key in ['amp_r', 'amp_i', 'f0','gbw','time']}
+    strace = inf_data['sd']
+    inf |= {key+'_sd': float(strace[key])
+            for key in ['amp_r', 'amp_i', 'f0','gbw','time']}
+    cpamp = inf['amp_r']+1.j*inf['amp_i']
+    inf['amp'] = np.abs(cpamp)
+    inf['phase'] = np.angle(cpamp)
+    inf['time'] = inf['time']
+    snip.set_infer(inf)
+    inf['snr'] = snr(snip)
+    inf['power'] = np.sum((snip.whts[256:768])**2)/512
+    inf['residual'] = np.sum(((snip.whts-snip.glitch)[256:768])**2)/512
+    inf['peak_frequency'] = float(freqs[(snip.invasd*np.abs(snip.fglitch)).argmax()])
+    return inf, snip
+
 class Snippet:
     """A class to load and whiten data, """
     def __init__(self, ifo, key, num):
@@ -58,6 +83,43 @@ class Snippet:
         """Whitened glitch time series"""
         ftmp = fglitch_from_sample(**self.inf)
         tmp = irfft(self.invasd * ftmp)
+        return center(tmp)
+
+    def plot(self):
+        myfreqs = np.linspace(0,4096,513)
+        
+        import matplotlib.pyplot as plt
+        fig1 = plt.figure()
+        plt.title(f"{self.ifo} {self.key} {self.num}")
+        plt.loglog(myfreqs, np.abs(rfft(self.whts)), c='k', lw=1)
+        plt.loglog(myfreqs, np.abs(rfft(self.glitch)), c='b', lw=1)
+        plt.loglog(myfreqs, np.abs(rfft(self.whts - self.glitch)), c='orange')
+        plt.ylim(1e-2,1e2)
+        
+        fig2 = plt.figure()
+        plt.title(f"{self.ifo} {self.key} {self.num}")
+        plt.plot(self.whts, c='k', lw=1)
+        plt.plot(self.whts - self.glitch, c='orange', ls=':')
+        
+        return fig1, fig2
+
+class SnippetNormed:
+    """A class to load and whiten data, """
+    def __init__(self, ifo, key, num):
+        self.ifo, self.key, self.num = ifo, key, num
+        npz = np.load(f"{datadir}/{ifo}-{key}-{num:04d}.npz")
+        self.invasd, self.whts, self.whts_long = extract_glitch(npz)
+    def set_infer(self, inf):
+        self.inf = inf
+    @property
+    def fglitch(self):
+        """Glitch centered at the start index"""
+        return fglitch_normed(self.invasd, **self.inf)
+    @property
+    def glitch(self):
+        """Whitened glitch time series"""
+        ftmp = fglitch_normed(self.invasd, **self.inf)
+        tmp = irfft(ftmp)
         return center(tmp)
 
     def plot(self):
